9eb454859528c94c42758e9b74ef64c97e4b8c7c
guodong94 <940470259@qq.com>
2019-03-25 23:58:30 +0800

    spring cloud sleuth

--- service-miya/src/main/java/com/gd/servicemiya/ServiceMiyaApplication.java
+++ service-miya/src/main/java/com/gd/servicemiya/ServiceMiyaApplication.java
@@ -1,29 +1,42 @@
-package com.gd.servicehi;
+package com.gd.servicemiya;
 
 import brave.sampler.Sampler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
-public class ServiceHiApplication {
+public class ServiceMiyaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceHiApplication.class, args);
+		SpringApplication.run(ServiceMiyaApplication.class, args);
 	}
 
-	private static Logger logger = LoggerFactory.getLogger(ServiceHiApplication.class);
+	private static final Logger log = LoggerFactory.getLogger(ServiceMiyaApplication.class);
+
+
+	@RequestMapping("/hi")
+	public String home(){
+		log.info("hi is being called");
+		return "hi i'm miya!";
+	}
+
+	@RequestMapping("/miya")
+	public String info(){
+		log.info("info is being called");
+		return restTemplate.getForObject("http://localhost:8101/info",String.class);
+	}
 
 	@Autowired
 	private RestTemplate restTemplate;
@@ -33,32 +46,10 @@ public class ServiceHiApplication {
 		return new RestTemplate();
 	}
 
-	@RequestMapping("/hi")
-	public String callHome(){
-		logger.info( "calling trace service-hi  ");
-		return restTemplate.getForObject("http://localhost:8300/miya", String.class);
-	}
-	@RequestMapping("/info")
-	public String info(){
-		logger.info("calling trace service-hi ");
-
-		return "i'm service-hi";
-
-	}
 
 	@Bean
 	public Sampler defaultSampler() {
 		return Sampler.ALWAYS_SAMPLE;
 	}
-
-	@Value("${server.port}")
-	String port;
-
-	@RequestMapping("/h1")
-	public String home(@RequestParam(value = "name",defaultValue = "gd") String name){
-
-		return "name:"+name+",port:"+port;
-	}
-
 
 }
